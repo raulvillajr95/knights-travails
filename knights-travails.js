1bb12d0fb12c736a8178f5a3cf7277695132bf78
Raul <raulvillajr95@gmail.com>
2023-05-29 21:34:04 -0700

    Solved knight travails, needs code improvement

--- knights-travails.js
+++ knights-travails.js
@@ -68,24 +68,45 @@ function shortestDistance(possiblesArr, endPosition) {
   return shortest[1];
 }
 
-function buildTree(rootPosition, currentBoard) {
-  let rootStr = `${rootPosition[0]}${rootPosition[1]}`;
-  let possibles = possibleMoves(rootPosition, currentBoard);
+function buildTree(rootPosition, currentBoard, endPosition) {
   let root = new Node(rootPosition);
-
+  let possibles = possibleMoves(rootPosition, currentBoard);
   for (let i = 0; i < possibles.length; i++) {
-    let possibleStr = `${possibles[i][[0]]}${possibles[i][1]}`;
-    root.children.push(new Node(possibles[i]));
+    let newNode = new Node(possibles[i]);
+
+    if (compareArrays(endPosition, possibles[i]))
+      console.log(rootPosition, endPosition);
+
+    let possibles2nd = possibleMoves(possibles[i], currentBoard);
+    for (let j = 0; j < possibles2nd.length; j++) {
+      let newNode2nd = new Node(possibles2nd[j]);
+
+      if (compareArrays(endPosition, possibles2nd[j]))
+        console.log(rootPosition, possibles[i], endPosition);
+
+      let possibles3rd = possibleMoves(possibles2nd[j], currentBoard);
+      for (let k = 0; k < possibles3rd.length; k++) {
+        let newNode3rd = new Node(possibles3rd[k]);
+
+        if (compareArrays(endPosition, possibles3rd[k]))
+          console.log(rootPosition, possibles[i], possibles2nd[j], endPosition);
+
+        newNode2nd.children.push(newNode3rd);
+      }
+
+      newNode.children.push(newNode2nd);
+    }
+
+    root.children.push(newNode);
   }
   return root;
 }
-function levelOrderCheck() {}
 
 function knightMoves(startPosition, endPosition) {
   let mainBoard = new Board(startPosition);
 
   // buildTree example
-  console.log(buildTree(startPosition, mainBoard));
+  console.log(buildTree(startPosition, mainBoard, endPosition));
 
   while (
     !compareArrays(endPosition, mainBoard.history[mainBoard.history.length - 1])
@@ -96,7 +117,7 @@ function knightMoves(startPosition, endPosition) {
   }
   return mainBoard.history;
 }
-console.log(knightMoves([0, 0], [1, 2]), 'kM');
+console.log(knightMoves([3, 3], [4, 3]), 'kM');
 
 function possibleMoves(arrXY, currentBoard) {
   let [x, y] = arrXY;
@@ -120,12 +141,6 @@ function possibleMoves(arrXY, currentBoard) {
   return arr;
 }
 
-let obj = {
-  graph: 'ox',
-  '00': 'hy',
-};
-console.log(obj['00']);
-
 /**
  * goal: input 2 positions [x1, y1], [x2, y2]
  * output shortest path to get there [[x1, y1]...[x2, y2]]
@@ -146,6 +161,10 @@ console.log(obj['00']);
  *    if there is, good
  *    if there isn't, check though depth of 2
  *    etc. keep checking at deeper levels
+ *  possibly create a rewind() function
+ *    moves knight one position back
+ *    makes board spot null, removes history, etc.
+ *  used queue for breath first search
  *
  *  examples:
  *  knightMoves([0,0],[1,2]) == [[0,0],[1,2]]
